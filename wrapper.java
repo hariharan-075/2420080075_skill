95c47a4a526e86227888a959390eff9d1f16d109
hariharan-075 <2420080075@klh.edu.in>
2025-08-02 09:29:02 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,12 @@
+package MyProject;
+
+public class wrapper {
+	public static void main(String args[]) {
+		Integer a=564;
+		Boolean b=true;
+		Double d=9.876d;
+		int c=10;
+		System.out.println(a.toString().length());
+		System.out.println(d.toString().length());
+	}
+}
